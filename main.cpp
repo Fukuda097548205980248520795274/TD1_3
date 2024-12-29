54762859a1384fbfeb8ab498eb97f5124bf3bcb7
k024g0083 <k024g0083@g.neec.ac.jp>
2024-12-30 07:27:27 +0900

    Playerクラスを作成した

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <Novice.h>
 #include "Constant.h"
+#include "./Class/Object/Player/Player.h"
 
 const char kWindowTitle[] = "LC1C_20_フクダソウワ_タイトル";
 
@@ -12,6 +13,18 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	// キー入力結果を受け取る箱
 	char keys[256] = {0};
 	char preKeys[256] = {0};
+
+
+	/*---------------
+	    変数を作る
+	---------------*/
+
+	// プレイヤー
+	Player* player = new Player();
+
+	// 画像
+	int ghWhite = Novice::LoadTexture("./NoviceResources/white1x1.png");
+
 
 	// ウィンドウの×ボタンが押されるまでループ
 	while (Novice::ProcessMessage() == 0) {
@@ -26,6 +39,8 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		/// ↓更新処理ここから
 		///
 
+		player->Operation(keys, preKeys);
+
 		///
 		/// ↑更新処理ここまで
 		///
@@ -33,6 +48,8 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 		/// ↓描画処理ここから
 		///
+
+		player->Draw(ghWhite);
 
 		///
 		/// ↑描画処理ここまで
@@ -46,6 +63,15 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 			break;
 		}
 	}
+
+
+	/*--------------------------
+	    インスタンスを削除する
+	--------------------------*/
+
+	// プレイヤー
+	delete player;
+
 
 	// ライブラリの終了
 	Novice::Finalize();
