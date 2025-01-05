4152ce9db39aa86eb0ddf3d3f0100614541ea8ef
k024g0083 <k024g0083@g.neec.ac.jp>
2025-01-06 07:51:55 +0900

    ブロックに耐久性をつけた

--- main.cpp
+++ main.cpp
@@ -5,17 +5,21 @@
 #include "./Class/Object/Player/Player.h"
 #include "./Class/Object/CarryBlock/Plastic/Plastic.h"
 #include "./Class/Object/CarryBlock/Treasure/Treasure.h"
+#include "./Class/Object/Enemy/Ghost/Ghost.h"
 
 const char kWindowTitle[] = "LC1C_20_フクダソウワ_タイトル";
 
 // マップ
-TILE Map::map_[kMapRow][kMapColumn];
+int Map::map_[kMapRow][kMapColumn];
 
 // プラスチック
 int Plastic::countID;
 
 // 宝
 int Treasure::countID;
+
+// ゴースト
+int Ghost::countID;
 
 
 // Windowsアプリでのエントリーポイント(main関数)
@@ -53,9 +57,76 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		treasure[i] = new Treasure();
 	}
 
-	plastic[0]->Putting(3, 3);
-	treasure[0]->Putting(3, 6);
+	// ゴースト
+	Ghost* ghost[kBlockNum];
+	for (int i = 0; i < kBlockNum; i++)
+	{
+		ghost[i] = new Ghost();
+	}
 
+	for (int row = 0; row < kMapRow; row++)
+	{
+		for (int column = 0; column < kMapColumn; column++)
+		{
+			switch (Map::map_[row][column])
+			{
+			case TILE_PLASTIC:
+				// プラスチック
+
+				for (int i = 0; i < kBlockNum; i++)
+				{
+					if (plastic[i]->id_ == 0)
+					{
+						plastic[i]->Putting(column, row);
+
+						break;
+					}
+				}
+
+				// タイルを消す
+				Map::map_[row][column] = TILE_NOTHING;
+
+				break;
+
+			case TILE_TREASURE:
+				// 宝
+
+				for (int i = 0; i < kBlockNum; i++)
+				{
+					if (treasure[i]->id_ == 0)
+					{
+						treasure[i]->Putting(column, row);
+
+						break;
+					}
+				}
+
+				// タイルを消す
+				Map::map_[row][column] = TILE_NOTHING;
+
+				break;
+
+			case TILE_GHOST:
+				// 幽霊
+
+				for (int i = 0; i < kBlockNum; i++)
+				{
+					if (ghost[i]->id_ == 0)
+					{
+						ghost[i]->Arrival(column, row);
+
+						break;
+					}
+				}
+
+				// タイルを消す
+				Map::map_[row][column] = TILE_NOTHING;
+
+				break;
+			}
+		}
+	}
+	
 	// 画像
 	int ghWhite = Novice::LoadTexture("./NoviceResources/white1x1.png");
 
@@ -73,21 +144,29 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		/// ↓更新処理ここから
 		///
 
+		// 腐らせる
+		Map::Rotten();
+
 		// 操作する
 		player->Operation(keys, preKeys);
 
+		// プレイヤーがブロックに乗る
 		for (int i = 0; i < kBlockNum; i++)
 		{
 			player->BlockLanding(plastic[i]);
 			player->BlockLanding(treasure[i]);
 		}
 
+		// ブロックを動かす
 		for (int i = 0; i < kBlockNum; i++)
 		{
 			plastic[i]->Move();
 			treasure[i]->Move();
+
+			ghost[i]->Move();
 		}
 
+		// ブロックの当たり判定
 		for (int i = 0; i < kBlockNum; i++)
 		{
 			for (int j = 0; j < kBlockNum; j++)
@@ -97,13 +176,31 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 				{
 					plastic[i]->BlockLanding(plastic[j]);
 					treasure[i]->BlockLanding(treasure[j]);
-					player->Carry(plastic[i] , plastic[j]);
-					player->Carry(treasure[i], treasure[j]);
 				}
 
 				// 別種類のブロック
 				plastic[i]->BlockLanding(treasure[j]);
 				treasure[i]->BlockLanding(plastic[j]);
+
+				ghost[i]->BlockLanding(plastic[j]);
+				ghost[i]->BlockLanding(treasure[j]);
+
+			}
+		}
+
+		// ブロックの当たり判定
+		for (int i = 0; i < kBlockNum; i++)
+		{
+			for (int j = 0; j < kBlockNum; j++)
+			{
+				// 同じ種類のブロック
+				if (i != j)
+				{
+					player->Carry(plastic[i], plastic[j]);
+					player->Carry(treasure[i], treasure[j]);
+				}
+
+				// 別種類のブロック
 				player->Carry(plastic[i], treasure[j]);
 				player->Carry(treasure[i], plastic[j]);
 
@@ -131,7 +228,10 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		{
 			plastic[i]->Draw(ghWhite);
 			treasure[i]->Draw(ghWhite);
+			ghost[i]->Draw(ghWhite);
 		}
+
+		Novice::ScreenPrintf(8, 8, "%d", treasure[1]->hp_);
 
 
 		///
@@ -159,6 +259,8 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	for (int i = 0; i < kBlockNum; i++)
 	{
 		delete plastic[i];
+		delete treasure[i];
+		delete ghost[i];
 	}
 
 	// ライブラリの終了
